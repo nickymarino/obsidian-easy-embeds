93752cb5bd1d62f06d4c2e00dea6a61cbc044d36
Nicky Marino <nicky@nickymarino.com>
2022-01-03 20:15:03 -0600

    Rename to Twitter Embeds; add publish blockquote support

--- main.ts
+++ main.ts
@@ -1,181 +1,53 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, MarkdownPostProcessorContext, MarkdownPostProcessor } from 'obsidian';
 
-import * as twitter from 'twitter'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+declare global {
+	interface Window {
+		twttr: any
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	twitterCodeBlockProcessor(
-		source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext
-	): void {
-		console.log('=======')
-		console.log(source);
-		console.log('=======')
-		console.log(el)
-		console.log('=======')
-		console.log(ctx)
-		el.textContent = 'your content: ' + source
-		twitter.loadWidgets()
-	}
-
-	postprocessor(el: HTMLElement, ctx: MarkdownPostProcessorContext): void {
-		console.log('hi 25')
-		twitter.loadWidgets();
-	}
-
+export default class TwitterEmbedsPlugin extends Plugin {
 
 	async onload() {
 		console.log("loading! hi :)")
-		this.registerMarkdownCodeBlockProcessor('twitter', this.twitterCodeBlockProcessor);
 
-		twitter.init()
-		this.registerMarkdownPostProcessor(this.postprocessor);
-		// this.app.workspace.on('layout-change', () => {
-		// 	// TODO: if any of this.app.workspace.getLayout() is in preview mode (or live edit?)
-		// 	console.log("reloading!")
-		// 	twitter.loadWidgets()
-		// }
-		// )
-
-
-		// const scriptNode = this.app.workspace.containerEl.createEl('script');
-		// scriptNode.src = 'https://platform.twitter.com/widgets.js'
-		// scriptNode.async = true
-		// scriptNode.onload = () => {
-		// 	console.log("i'm loaded!")
-		// 	twttr.widgets.load()
-		// }
-
-		// this.app.workspace.containerEl.appendChild(scriptNode)
-
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		this.loadTwitterIfNotAlreadyLoaded()
+		this.registerMarkdownPostProcessor(async (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			await this.renderTwitterWidgets()
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private loadTwitterIfNotAlreadyLoaded() {
+		// https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+		window.twttr = (function (d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0],
+				t = window.twttr || {};
+			if (d.getElementById(id)) return t;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "https://platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js, fjs);
+
+			t._e = [];
+			t.ready = function (f) {
+				t._e.push(f);
+			};
+
+			console.log("hello, I'm loading the widget")
+
+			return t;
+		}(document, "script", "twitter-wjs"));
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	private renderTwitterWidgets() {
+		window.twttr.widgets.load()
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
